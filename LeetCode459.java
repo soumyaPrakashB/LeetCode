c8b9663153e2df5946e382593fcb3cd625ce2525
Soumya Behera <lpsoumyaprakash@gmail.com>
2023-08-21 13:24:52 +0530

    solution to daily challenge 21st aug, 2023

--- LeetCode459.java
+++ LeetCode459.java
@@ -1,2 +1,32 @@
 public class LeetCode459 {
+    public boolean repeatedSubstringPattern(String s) {
+
+        for(int i = 1; i <= s.length()/2; i++) {
+            String current = s.substring(0, i);
+            int j = 0;
+
+            while(j <= s.length()) {
+                if(j == s.length()) {
+                    return true;
+                }
+                if(j + i > s.length()) {
+                    break;
+                }
+                if(current.equals(s.substring(j, j + i))) {
+                    j = j + i;
+                }
+                else {
+                    break;
+                }
+            }
+
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LeetCode459 leetCode459 = new LeetCode459();
+        System.out.println(leetCode459.repeatedSubstringPattern("aaaaaaaac"));
+    }
 }
